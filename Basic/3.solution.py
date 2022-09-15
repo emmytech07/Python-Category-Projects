eaaf87df58d1b81f505c87186d68ea3df8e9a155
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-15 05:04:14 +0100

    Solution to question 3

---
+++ Basic/3.solution.py
@@ -0,0 +1,10 @@
+#display a character at the even indices
+
+# Solution to question 2
+
+def print_even_index(str):
+    for char in range(0, len(str), 2):
+        print(f"Index [", char,"]", str[char])
+
+string = "Computer Study"
+print_even_index(string)
