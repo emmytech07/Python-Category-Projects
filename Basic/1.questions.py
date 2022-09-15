2fcbf966126d3bbd08d4eedf2089c733c14bbb30
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-15 03:39:42 +0100

    Question 1 solved

---
+++ Basic/1.questions.py
@@ -0,0 +1,12 @@
+# Print  the result of the multiplication of two set of integers, if it less than 1000. if the results is greater than 1000, print sum 
+
+num1 = 32
+num2 = 220
+
+if((num1 * num2) <1000):
+    product = (num1 * num2 )
+    print(product)
+
+else:
+    sum = num1 + num2
+    print( sum)
---
+++ Basic/1.solutions.py
@@ -0,0 +1,13 @@
+
+def calculations(num1, num2):
+    product = num1 * num2
+    if product < 1000:
+        return product
+    else:
+        sum = num1 + num2
+        return sum
+
+value = calculations(200, 2)
+value1 = calculations(76, 20)
+print(value)
+print(value1)
